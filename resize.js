Iwasawafag/jimp

commit 65158a985264ee2014e49b7514823f9725394674

Fix error in Resize.resizeWidthInterpolatedRGBA

resize.js
@@ -177,7 +177,7 @@ Resize.prototype.resizeWidthInterpolatedRGBA = function (buffer) {
     var secondWeight = 0;
     var outputBuffer = this.widthBuffer;
     //Handle for only one interpolation input being valid for start calculation:
-    for (var targetPosition = 0; weight < 1 / 3; targetPosition += 4, weight += ratioWeight) {
+    for (var targetPosition = 0; weight < 1 / 4; targetPosition += 4, weight += ratioWeight) {
         for (finalOffset = targetPosition, pixelOffset = 0; finalOffset < this.widthPassResultSize; pixelOffset += this.originalWidthMultipliedByChannels, finalOffset += this.targetWidthMultipliedByChannels) {
             outputBuffer[finalOffset] = buffer[pixelOffset];
             outputBuffer[finalOffset + 1] = buffer[pixelOffset + 1];

commit 8c27ae56243dd75f526dd6c36428265c45ef7c5b

Fix error introduced in 65158a9

resize.js
@@ -86,9 +86,9 @@ Resize.prototype._resizeWidthInterpolatedRGBChannels = function (buffer, fourthC
         for (finalOffset = targetPosition, pixelOffset = Math.floor(weight) * channelsNum; finalOffset < this.widthPassResultSize; pixelOffset += this.originalWidthMultipliedByChannels, finalOffset += this.targetWidthMultipliedByChannels) {
             outputBuffer[finalOffset] = (buffer[pixelOffset] * firstWeight) + (buffer[pixelOffset + channelsNum] * secondWeight);
             outputBuffer[finalOffset + 1] = (buffer[pixelOffset + 1] * firstWeight) + (buffer[pixelOffset + channelsNum + 1] * secondWeight);
-            outputBuffer[finalOffset + 2] = (buffer[pixelOffset + 2] * firstWeight) + (buffer[pixelOffset + channelsNum + 1] * secondWeight);
+            outputBuffer[finalOffset + 2] = (buffer[pixelOffset + 2] * firstWeight) + (buffer[pixelOffset + channelsNum + 2] * secondWeight);
             if (!fourthChannel) continue;
-            outputBuffer[finalOffset + 3] = (buffer[pixelOffset + 3] * firstWeight) + (buffer[pixelOffset + channelsNum + 1] * secondWeight);
+            outputBuffer[finalOffset + 3] = (buffer[pixelOffset + 3] * firstWeight) + (buffer[pixelOffset + channelsNum + 3] * secondWeight);
         }
     }
     //Handle for only one interpolation input being valid for end calculation: